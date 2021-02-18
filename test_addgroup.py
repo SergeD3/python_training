56704c25f99f78bb2078327dc036237892412e5a
Серж <findhall437@mail.ru>
2021-02-18 22:12:18 +0300

    Заработало!!

--- test_addgroup.py
+++ test_addgroup.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class Test1(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
