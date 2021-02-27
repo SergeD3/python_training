5a11fcbb04c07c9cbf97a98c49637a95721a2cc4
Сергей <34037917+SergeD3@users.noreply.github.com>
2021-02-27 11:55:52 +0300

    полная переработка методов и переезд в pytest + создание нового класса и релокация методов. Проверил, тесты успешно запускаются.

--- test_addgroup.py
+++ test_addgroup.py
@@ -1,86 +1,27 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class Test1(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_group(self):
-        wd = self.wd
-        self.login(user_name="admin", password="secret")
-        wd.find_element_by_link_text("groups").click()
-        self.create_group(Group(name="new group2", header="qwerty", footer="qwerty"))
-        self.open_group_page()
-        self.logout()
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.login(user_name="admin", password="secret")
-        wd.find_element_by_link_text("groups").click()
-        self.create_group(Group(name="", header="", footer=""))
-        self.open_group_page()
-        self.logout()
-
-    def create_group(self, group):
-        # create new group
-        wd = self.wd
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-
-    def logout(self):
-        # logout
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def login(self, user_name, password):
-        # login
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user_name)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        # open home page
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+        app.login(user_name="admin", password="secret")
+        app.open_group_page()
+        app.create_group(Group(name="new group2", header="qwerty", footer="qwerty"))
+        app.open_group_page()
+        app.logout()
+
+
+def test_add_empty_group(app):
+        app.login(user_name="admin", password="secret")
+        app.open_group_page()
+        app.create_group(Group(name="", header="", footer=""))
+        app.open_group_page()
+        app.logout()
