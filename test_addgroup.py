8dfff815ba397fd490d2fafab0e0cd1a2d31e805
Сергей <34037917+SergeD3@users.noreply.github.com>
2021-02-27 11:14:24 +0300

    доработка методов и улучшение читабельности кода.

--- test_addgroup.py
+++ test_addgroup.py
@@ -13,28 +13,27 @@ class Test1(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, user_name="admin", password="secret")
+        self.login(user_name="admin", password="secret")
         wd.find_element_by_link_text("groups").click()
-        self.create_group(wd, Group(name="new group2", header="qwerty", footer="qwerty"))
-        wd.find_element_by_name("submit").click()
-        # submit group creation
+        self.create_group(Group(name="new group2", header="qwerty", footer="qwerty"))
+        self.open_group_page()
+        self.logout()
+
+    def open_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
-        self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, user_name="admin", password="secret")
+        self.login(user_name="admin", password="secret")
         wd.find_element_by_link_text("groups").click()
-        self.create_group(wd, Group(name="", header="", footer=""))
-        wd.find_element_by_name("submit").click()
-        # submit group creation
-        wd.find_element_by_link_text("group page").click()
-        self.logout(wd)
+        self.create_group(Group(name="", header="", footer=""))
+        self.open_group_page()
+        self.logout()
 
-    def create_group(self, wd, group):
+    def create_group(self, group):
         # create new group
+        wd = self.wd
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -45,13 +44,17 @@ class Test1(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
 
-    def logout(self, wd):
+    def logout(self):
         # logout
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def login(self, wd, user_name, password):
+    def login(self, user_name, password):
         # login
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(user_name)
@@ -60,8 +63,9 @@ class Test1(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
         # open home page
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
