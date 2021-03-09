22d24263acb75989da05ad29e984a8b951d9fe11
Сергей <34037917+SergeD3@users.noreply.github.com>
2021-03-09 22:09:54 +0300

    Выделил вспом. класс SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,6 +7,7 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_group_page(self):
         wd = self.wd
@@ -27,23 +29,6 @@ class Application:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_xpath("/html/body/div/div[4]/div/i/a").click()
-
-    def logout(self):
-        # logout
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def login(self, user_name, password):
-        # login
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user_name)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self):
         # open home page
--- tests/test_addgroup.py
+++ tests/test_addgroup.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-        app.login(user_name="admin", password="secret")
+        app.session.login(user_name="admin", password="secret")
         app.create_group(Group(name="new group2", header="qwerty", footer="qwerty"))
-        app.logout()
+        app.session.logout()
 
 
 def test_add_empty_group(app):
-        app.login(user_name="admin", password="secret")
+        app.session.login(user_name="admin", password="secret")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
